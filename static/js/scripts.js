a8bcaea0fd6e344958a01c807861d7fedc0c0733
tykva43 <krikunova.olga@mail.ru>
2021-03-18 16:14:51 +0700

    Added adaptive content width changing'

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,12 @@
-var left_menu_width = 300;
+var is_menu_opened = 0;
+/*var left_menu_width = 300;
 var left_menu_margin = 0;
-var left_min_menu = 50;
+var left_min_menu_width = 50;
 var slider_width = 160;
-var width_of_others = 140;
-var content_padding_width = 25;
-var random_colors = ['#FFB6B9', '#BBDED6', '#29F087', '#A73DE3', '#E33927', ];
-var content_left_margin = left_min_menu;
+var outer_width = 140;
+var content_padding_width = 25;*/
+//var random_colors = ['#FFB6B9', '#BBDED6', '#29F087', '#A73DE3', '#E33927', ];
+//var content_left_margin = left_min_menu_width;
 
 function div(val, by){
     return (val - val % by) / by;
@@ -13,49 +14,16 @@ function div(val, by){
 
 $(document).ready(function() {
 
+    changeContentWidth();
+
     $('.expander').on('click', function() {
         $(this).toggleClass('rotated');
         $('.groups_list').toggleClass('invisible');
     });
 
-    $('.menu_item').on( 'click', function() {
+    $('.menu_item').on('click', menuItemClicked);
 
-        $('#flip_toggle').toggleClass('flip');
-
-        if ($('.left_menu.pop_up_menu').css('margin-left') == left_min_menu.toString() + "px") { // если левое меню выдвинуто
-            // задвинуть левое меню
-            $('.left_menu.pop_up_menu').css('margin-left', (-left_menu_width).toString() + 'px');
-
-            // изменить ширину для уменьшения контента
-            width_of_others += left_menu_width
-            // изменить маргин контента
-            content_left_margin -= left_menu_width
-            changeContentWidth();
-        }
-        else {
-            // вытащить левое меню
-            $('.left_menu.pop_up_menu').css('margin-left', left_min_menu.toString() + "px");
-/*
-            // изменить ширину для уменьшения контента
-            width_of_others = left_menu_width;
-            // изменить маргин контента
-            content_left_margin += left_menu_width;
-            changeContentWidth();*/
-        }
-
-        if ($('.left_menu').css('margin-left') == '50px') {
-            left_menu_margin += 300;
-            console.log(left_menu_margin);
-            }
-        else {
-            left_menu_margin -= 300;
-            console.log(left_menu_margin);
-            }
-    });
-
-    changeContentWidth();
-
-    $(window).on( "resize", changeContentWidth );
+    $(window).on( "resize", changeContentWidth);
 
     $("[data-tooltip]").mousemove(function(event) {
         $data_tooltip = $(this).attr("data-tooltip");
@@ -84,7 +52,79 @@ $(document).ready(function() {
 });
 
 function changeContentWidth() {
-        $( ".content" ).css("width", ( $(window).width() - left_min_menu - width_of_others ).toString()+ "px");
-        $('.left_menu').addClass('pop_up_menu');
-        $('.left_menu.pop_up_menu').css('margin-left', left_menu_margin.toString() + 'px');
+    console.log('***** changeContentWidth *****')
+
+    var left_min_menu_width = $('.left_min_menu').width();
+    var content_outer_width = $('.content').innerWidth() - $('.content').width();
+    var left_menu_width = is_menu_opened * $('.left_menu').width();
+//    var content_left_margin = left_menu_width + left_min_menu_width + ($('.content').outerWidth(true) - $('.content').outerWidth())/2;
+    var content_left_margin = left_menu_width;
+
+    console.log('content_outer_width = ' + content_outer_width.toString())
+    console.log('left_menu_width = ' + left_menu_width.toString())
+    console.log('content_left_margin = ' + content_left_margin.toString())
+
+    $(".content").css("width", ( $(window).width() - content_outer_width - left_menu_width - left_min_menu_width).toString()+ "px");
+    // $('.left_menu').addClass('pop_up_menu');
+    // $('.left_menu.pop_up_menu').css('margin-left', left_menu_margin.toString() + 'px');
+    // $('.left_menu.pop_up_menu').css('margin-left', left_menu_margin.toString() + 'px');
+    $('.content').css('margin-left', content_left_margin.toString() + 'px');
+
 }
+
+function menuItemClicked() {
+
+    $('#flip_toggle').toggleClass('flip');
+    $('.left_menu').toggleClass('pop_up_menu');
+
+
+
+    var left_min_menu_width = $('.left_min_menu').width();
+    var left_menu_width = $('.left_menu').width();
+
+    if ($('.left_menu').css('margin-left') == $('.left_min_menu').width().toString() + "px") { // если левое меню выдвинуто
+        // задвинуть левое меню
+        is_menu_opened = 0;
+        $('.left_menu').css('margin-left', (-left_menu_width).toString() + 'px');
+
+//        // изменить ширину для уменьшения контента
+//        width_of_others += left_menu_width
+//        // изменить маргин контента
+//        content_left_margin -= left_menu_width
+        changeContentWidth();
+    }
+    else {
+        // вытащить левое меню
+        is_menu_opened = 1;
+//        $('.left_menu').css('margin-left', left_min_menu_width.toString() + "px");
+        $('.left_menu').css('margin-left', left_min_menu_width.toString() + "px");
+        changeContentWidth();
+/*
+            // изменить ширину для уменьшения контента
+            width_of_others = left_menu_width;
+            // изменить маргин контента
+            content_left_margin += left_menu_width;
+            changeContentWidth();*/
+        }
+
+        console.log('***** menuItemClicked *****')
+//    var left_min_menu_width = $('.left_min_menu').width();
+//    var content_outer_width = $('.content').outerWidth(true) - $('.content').width();
+//    var left_menu_width = is_menu_opened * $('.left_menu').width();
+////    var content_left_margin = left_menu_width + left_min_menu_width + ($('.content').outerWidth(true) - $('.content').outerWidth())/2;
+//    var content_left_margin = left_menu_width + left_min_menu_width + ($('.content').outerWidth(true) - $('.content').outerWidth())/2;
+//
+//    console.log('left_min_menu_width = ' + left_min_menu_width.toString())
+//    console.log('content_outer_width = ' + content_outer_width.toString())
+//    console.log('left_menu_width = ' + left_menu_width.toString())
+//    console.log('content_left_margin = ' + content_left_margin.toString())
+/*
+        if ($('.left_menu').css('margin-left') == '50px') {
+            left_menu_margin += 300;
+            console.log(left_menu_margin);
+            }
+        else {
+            left_menu_margin -= 300;
+            console.log(left_menu_margin);
+            }*/
+    }
